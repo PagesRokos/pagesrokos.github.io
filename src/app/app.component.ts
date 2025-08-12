b088aa6443af89f32101074b49e67b90b5e32711
sebasilvanavea <sebastian.silvanavea@gmail.com>
2025-08-11 22:49:39 -0400

    feat: delay aparición FABs + tamaño icono WhatsApp y preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { LoyaltyPopupComponent } from "./components/loyalty-popup/loyalty-popup.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,19 @@ import { LoyaltyPopupComponent } from "./components/loyalty-popup/loyalty-popup.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] // Fixed typo
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rokos-barber-club';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        document.body.classList.add('app-loaded');
+        const preloader = document.getElementById('app-preloader');
+        if (preloader) {
+          preloader.style.opacity = '0';
+          setTimeout(() => preloader.remove(), 500);
+        }
+      });
+    }
+  }
 }
